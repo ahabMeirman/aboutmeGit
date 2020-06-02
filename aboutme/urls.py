560c790504e7e84bd26d47005782ae0485730933
ahabMeirman <meirman_ahab_90@mail.ru>
2020-06-02 12:11:17 +0600

    install ckeditor

--- aboutme/urls.py
+++ aboutme/urls.py
@@ -16,16 +16,18 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from aboutme.views import home
+# File Upload:
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('ckeditor/', include('ckeditor_uploader.urls')),# обязательно в корневом файле подключение
     path('home/', home, name='home_url'),
     path('myblog/', include('myblog.urls')),
     path('resume/', include('resume.urls')),
 ]
-# File Upload:
-from django.conf import settings
-from django.conf.urls.static import static
+
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- myblog/admin.py
+++ myblog/admin.py
@@ -1,5 +1,23 @@
 from django.contrib import admin
 from .models import *
-# Register your models here.
+from ckeditor_uploader.widgets import CKEditorUploadingWidget#загрузка изоброжении
+from django import forms
 
-admin.site.register(Blog)
+
+# Blog и все его настройки в админ панели, плюс виджет который отвечает за отоброжение 
+#всех имеющихся инструментов плагина CKEditor________________________________________
+class BlogAdminForm(forms.ModelForm):
+    text = forms.CharField(widget=CKEditorUploadingWidget())
+    class Meta:
+        model = Blog
+        fields = '__all__'
+
+class BlogAdmin(admin.ModelAdmin):
+
+	list_display = ('title', 'text', 'date',)
+	search_fields = ('__str__',)
+	form = BlogAdminForm
+
+# настройки ckeditora закончились========
+
+admin.site.register(Blog, BlogAdmin)
